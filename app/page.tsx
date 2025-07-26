9c8f432331c8e5e766812432f9e89d77a847297c
Pyo Hyeon <megatwins@kaist.ac.kr>
2025-07-27 00:17:00 +0900

    feat: add new app demo images

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,17 @@ export default async function Page() {
                             TRIP TODO 와 함께 간편하게 여행을 준비해보세요
                         </Text></>} image={<ImagePlaceHolder flex={1} flexBasis={'auto'} />} />
             <FeatureSummarySection />
-            <LandingPageSection title={<>티켓과 예약 내역이 필요할 때<br />간편하게 꺼내보기</>} body={<>인터넷 브라우저, 항공사 앱, 숙소 예약 앱, 종이 티켓에 담아다니던 예약 내역<br />이젠 한 곳에 모아 여행 중 클릭 한번으로 이용해보세요</>} image={<ImagePlaceHolder />} />
+            <LandingPageSection title={<>티켓과 예약 내역이 필요할 때<br />간편하게 꺼내보기</>} body={<>인터넷 브라우저, 항공사 앱, 숙소 예약 앱, 종이 티켓에 담아다니던 예약 내역<br />이젠 한 곳에 모아 여행 중 클릭 한번으로 이용해보세요</>} image={
+                <Image
+                    alt={'Hero Image'}
+                    fit={'cover'}
+                    align={'center'}
+                    w={'100%'}
+                    h={'100%'}
+                    src={
+                        'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                    }
+                />} />
             <TicketFeatureSection />
             <LandingPageSection title={<>일일이 옮겨 적을 필요 없이<br />화면 캡쳐만으로 내 예약 관리하기</>} body={<>일일이 옮겨 적을 필요 없이 화면 캡쳐만으로 내 예약 관리하기</>} image={<ImagePlaceHolder />} />
             <Flex hideFrom={'md'} position={'fixed'} zIndex={1000} bottom={0} left={0} width={'100%'} pr={5} pl={5} pt={5} pb={8} >
